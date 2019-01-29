ba6b88995946eddb7d171485918a29e98ca55138
Maeva Pasteur <maeva.pasteur@hetic.net>
2019-01-29 14:03:58 +0100

    Feat input range on design page

--- src/app/graphic/design/design.component.ts
+++ src/app/graphic/design/design.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import * as $ from 'jquery';
+import {DataService} from '../../data.service';
 
 @Component({
   selector: 'app-design',
@@ -7,9 +9,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DesignComponent implements OnInit {
 
-  constructor() { }
+  timeAsk: any = 1;
+
+  constructor(private data: DataService) { }
 
   ngOnInit() {
+
+      $('.range-slider__range.sumAsk').on('input', function () {
+          initInputRange($(this), '.text.sumAsk', 750);
+      });
+
+      $('.range-slider__range.timeAsk').on('input', function () {
+          initInputRange($(this), '.text.timeAsk', 12);
+      });
+
+      function initInputRange(e, p, valueMax) {
+          $(p).html(e.val());
+          let left = e.val() * (e.width() / valueMax);
+          e.closest('.range-slider').find('.line-input').css('left', left);
+      }
   }
 
 }
