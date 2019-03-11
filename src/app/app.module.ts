16c6492726af2394511a6e5739776edc190cac7c
Maeva Pasteur <maeva.pasteur@hetic.net>
2019-03-11 14:07:03 +0100

    Feat new chatbot

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule }   from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -42,6 +43,7 @@ import { WireframeComponent } from './graphic/wireframe/wireframe.component';
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ViewChild, ElementRef, OnInit } from '@angular/core';
+import * as $ from 'jquery';
+import { element } from 'protractor';
+import { BootService } from '../boot.service';
+
+
+export interface Message {
+    remetente?: string;
+    mensagem: string;
+    data?: Date;
+}
 
 @Component({
   selector: 'app-home',
@@ -6,10 +16,51 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+    ngOnInit(): void {
+    }
 
-  constructor() { }
+    @ViewChild('scrollMe') private myScrollContainer: ElementRef;
+    msg: string;
+    resultados: Message[];
 
-  ngOnInit() {
-  }
+    constructor(private chatBoot: BootService) {
+        this.initBoot();
+    }
+
+    initBoot() {
+        this.resultados = [];
+        this.chatBoot.getResponse('oi')
+            .subscribe((lista: any) => {
+                lista.result.fulfillment.messages.forEach((element) => {
+                    this.resultados.push({ remetente: 'boot', mensagem: element.speech, data: lista.timestamp });
+                });
+            });
+    }
+
+    sendMessage() {
+        this.resultados.push({ remetente: 'eu', mensagem: this.msg, data: new Date() });
+        this.chatBoot.getResponse(this.removerAcentos(this.msg))
+            .subscribe((lista: any) => {
+                lista.result.fulfillment.messages.forEach((element) => {
+                    this.resultados.push({ remetente: 'boot', mensagem: element.speech, data: lista.timestamp });
+                });
+            });
+
+        this.msg = '';
+    }
+
+    ngAfterViewChecked() {
+        this.scrollToBottom();
+    }
+
+    scrollToBottom(): void {
+        try {
+            this.myScrollContainer.nativeElement.scrollTop = this.myScrollContainer.nativeElement.scrollHeight;
+        } catch (err) { }
+    }
+
+    private removerAcentos(s) {
+        return s.normalize('NFD').replace(/[\u0300-\u036f]/g, '');
+    }
 
 }
